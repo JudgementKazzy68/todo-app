d448e237d83641fdde62584dd986847cb3aecb59
JudgementKazzy68 <kazzyjudgement68@gmail.com>
2025-01-23 15:12:22 +0100

    Complete todo app
    Items can be marked as complete/incomplete and
    can be deleted.
    
    Refactoring still needed, and commenting.

--- scripts.js
+++ scripts.js
@@ -4,12 +4,14 @@ const listMain = document.querySelector(".list-main");
 const inputField = document.querySelector(".input-field");
 let listItems = document.getElementsByTagName("li");
 let checkBoxes = document.getElementsByClassName("complete-task");
+let deleteButtons = document.getElementsByClassName("btn-delete");
 
-function addItem() {
+function appendItem() {
   const li = document.createElement("li");
   const nav = document.createElement("nav");
   const checkBox = document.createElement("input");
   const div = document.createElement("div");
+  let button = document.createElement("button");
   listMain.appendChild(li);
   li.appendChild(nav);
   nav.classList.add("list-item");
@@ -17,20 +19,51 @@ function addItem() {
   checkBox.type = "checkbox";
   checkBox.classList.add("complete-task");
   checkBox.addEventListener("click", function () {
-    for (let i = 0; i < checkBoxes.length; i++) {
-      if (checkBoxes[i].checked === true) {
-        listItems[i].remove();
-      }
-    }
+    div.classList.toggle("completed");
   });
   nav.appendChild(div);
   div.textContent = inputField.value;
+  nav.appendChild(button);
+  button.textContent = "🚫";
+  button.classList.add("btn-delete");
+  button.addEventListener("click", function () {
+    button.parentNode.parentNode.remove();
+  });
   inputField.value = "";
-  console.log(checkBoxes);
+}
+
+function popItem() {
+  const li = document.createElement("li");
+  const nav = document.createElement("nav");
+  const checkBox = document.createElement("input");
+  const div = document.createElement("div");
+  let button = document.createElement("button");
+  listMain.insertBefore(li, listItems[0]);
+  li.appendChild(nav);
+  nav.classList.add("list-item");
+  nav.appendChild(checkBox);
+  checkBox.type = "checkbox";
+  checkBox.classList.add("complete-task");
+  checkBox.addEventListener("click", function () {
+    div.classList.toggle("completed");
+  });
+  nav.appendChild(div);
+  div.textContent = inputField.value;
+  nav.appendChild(button);
+  button.textContent = "🚫";
+  button.classList.add("btn-delete");
+  button.addEventListener("click", function () {
+    button.parentNode.parentNode.remove();
+  });
+  inputField.value = "";
 }
 
 inputField.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
-    addItem();
+    if (listItems.length === 0) {
+      appendItem();
+    } else {
+      popItem();
+    }
   }
 });
